a5a96e5526a9483019b23615b9775be9abc9a9ac
= <user.email>
2020-06-10 11:45:25 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# base requirements
+install_requires = open("requirements.txt").read().strip().split("\n")
+
+setup(
+    name='codema_sec_mentors',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    version='0.0.1',
+    author='Codema',
+    author_email='rowan.molony@codema.ie',
+    description='Codema SEC mentor Project Management code',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rdmolony/codema_sec_mentors',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # 'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6',
+)
+
+'''Sources
+    https://packaging.python.org/tutorials/packaging-projects/#setup-py
+    Python Testing with Pytest
+    '''
