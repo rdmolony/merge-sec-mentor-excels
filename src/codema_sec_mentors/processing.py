daa851d3c33f57f0bb359df46c38d6f2ff242727
= <user.email>
2020-06-11 17:23:26 +0100

    ETL total hours from mentor excels & delete...
    Functions to rename duplicate columns,
    remove rows above headers etc. all removed
    as not needed in final ETL

--- src/codema_sec_mentors/processing.py
+++ src/codema_sec_mentors/processing.py
@@ -30,14 +30,14 @@ def process_mentor_excels() -> Flow:
         )
 
         hourly_overview_clean = (
-            _drop_rows_before_headers.map(hourly_overview_raw)
-            >> _drop_empty_rows.map()
-            >> _set_first_row_as_column_headers.map()
-            >> _rename_duplicate_column_names.map()
+            _drop_empty_rows.map(hourly_overview_raw)
             >> _get_rlpdt_totals.map()
+            >> _merge_mentor_dataframes
         )
 
-        _save_totals_to_excel(hourly_overview_clean, RESULTS_DIR, filepaths)
+        _save_merged_dataframe_to_excel(
+            hourly_overview_clean, RESULTS_DIR, "mentor_hours.xlsx"
+        )
 
     return flow
 
@@ -63,18 +63,6 @@ def _get_excel_filepaths(directory) -> List[Path]:
 
 
 @task
-def _load_sec_activity_by_month_sheet(filepath: Path) -> pd.DataFrame:
-
-    sheet_name = "SEC activity by month"
-
-    # NOTE: this function fails for some LAs as the headers are
-    # typically 6 or 7 rows deep
-    return pd.read_excel(
-        filepath, sheet_name=sheet_name, header=None, engine="openpyxl",
-    )
-
-
-@task
 def _load_sec_activity_by_month_sheet_inferring_headers(filepath: Path) -> pd.DataFrame:
 
     sheet_name = "SEC activity by month"
@@ -87,10 +75,11 @@ def _load_sec_activity_by_month_sheet_inferring_headers(filepath: Path) -> pd.Da
     while header_row_not_found:
 
         row = sheet[row_number]
-
+        row_values = [cell.value for cell in row[:10]]
         # search first 10 columns...
-        if "SEC Name" in (cell.value for cell in row[:10]):
+        if "SEC Name" in row_values:
             header_row_not_found = False
+            break
 
         # If not found in first 20 rows...
         if row_number == 20:
@@ -100,52 +89,8 @@ def _load_sec_activity_by_month_sheet_inferring_headers(filepath: Path) -> pd.Da
 
     # Load excel sheet using headers found above...
     return pd.read_excel(
-        filepath, sheet_name=sheet_name, header=row_number, engine="openpyxl"
+        filepath, sheet_name=sheet_name, header=row_number - 1, engine="openpyxl"
     )
-
-
-@task
-def _drop_rows_based_on_na_values_in_first_column(
-    excel_df: pd.DataFrame,
-) -> pd.DataFrame:
-    """As the column headers are typically in rows 6 or 7 need to drop
-    all rows prior to these headers.  This will fail if values in column A
-    are non-empty prior to headers...
-
-    Parameters
-    ----------
-    raw_excel_df : pd.DataFrame
-
-    Returns
-    -------
-    pd.DataFrame
-    """
-
-    return excel_df[excel_df[0].notna()]
-
-
-@task
-def _drop_rows_before_headers(excel_df: pd.DataFrame,) -> pd.DataFrame:
-    """As the column headers are typically in rows 6 or 7 need to drop
-    all rows prior to these headers.  This function looks for the first 
-    occurance of a row containing 'SEC Name' and removes the rows above it
-
-    Parameters
-    ----------
-    raw_excel_df : pd.DataFrame
-
-    Returns
-    -------
-    pd.DataFrame
-    """
-
-    import ipdb
-
-    ipdb.set_trace()
-
-    index_of_header_row = excel_df[excel_df.eq("SEC Name").any(1)].index[0]
-
-    return excel_df.iloc[index_of_header_row:]
 
 
 @task
@@ -162,31 +107,7 @@ def _drop_empty_rows(excel_df: pd.DataFrame,) -> pd.DataFrame:
     pd.DataFrame
     """
 
-    return excel_df[excel_df[1].notna()]
-
-
-@task
-def _set_first_row_as_column_headers(excel_df: pd.DataFrame,) -> pd.DataFrame:
-
-    excel_df.columns = excel_df.iloc[0]
-
-    return excel_df.drop(excel_df.index[0])
-
-
-@task
-def _rename_duplicate_column_names(excel_df: pd.DataFrame,) -> pd.DataFrame:
-
-    # NOTE: https://stackoverflow.com/questions/40774787/renaming-columns-in-a-pandas-dataframe-with-duplicate-column-names
-
-    renamer = defaultdict()
-
-    for col in excel_df.columns[excel_df.columns.duplicated(keep=False)].tolist():
-        if col not in renamer:
-            renamer[col] = [col + "_0"]
-        else:
-            renamer[col].append(col + "_" + str(len(renamer[col])))
-
-    return excel_df.rename(columns=lambda c: renamer[c].pop(0) if c in renamer else c)
+    return excel_df[excel_df.iloc[:, 1].notna()]
 
 
 @task
@@ -194,29 +115,20 @@ def _get_rlpdt_totals(excel_df: pd.DataFrame,) -> pd.DataFrame:
 
     excel_df = excel_df.set_index("SEC Name")
 
-    rlpdt_totals = ["Recruit_0", "Learn_0", "Plan_0", "Do_0", "Total"]
+    rlpdt_totals = ["Recruit", "Learn", "Plan", "Do", "Total"]
 
     return excel_df[rlpdt_totals]
 
 
 @task
-def _save_totals_to_excel(
-    excel_df: pd.DataFrame, savedir: Path, filepath: Path
-) -> None:
+def _merge_mentor_dataframes(excel_dfs: List[pd.DataFrame]) -> pd.DataFrame:
 
-    excel_df.to_excel(savedir / f"{filepath.stem}.xlsx")
+    return pd.concat(excel_dfs)
 
 
-# Roughwork
-"""
-column_names = [
-    "SEC Name",
-    "County / LA",
-    "Recruit",
-    "Learn",
-    "Plan",
-    "Do",
-    "L-P-D",
-    "EMP application",
-]
-"""
+@task
+def _save_merged_dataframe_to_excel(
+    merged_df: pd.DataFrame, savedir: Path, filename: str
+) -> pd.DataFrame:
+
+    merged_df.to_excel(savedir / filename)
