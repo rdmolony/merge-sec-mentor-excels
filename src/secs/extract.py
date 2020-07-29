d7babf87fdfe161c96d8cf79328e17aec881f75c
Rowan Molony <rowan.molony@codema.ie>
2020-07-29 15:10:38 +0100

    Log filepaths & Ignore all SEC(1).xlsx etc files
    (1) Log all filepaths to console so can see what excels
    are being read in (and alter extract filter accordingly
    if needed)
    
    (2) Had a simple if "(1)" in filename which is not
    as robust as regex which works for all filenames with
    numbers in brackets if multiple duplicates ...

--- src/secs/extract.py
+++ src/secs/extract.py
@@ -1,31 +1,44 @@
+import re
 from pathlib import Path
 from typing import Dict, List
 
-from cytoolz.dicttoolz import merge_with
-
-# from toolz.dicttoolz import merge_with
 import icontract
 import numpy as np
 import pandas as pd
+from cytoolz.dicttoolz import merge_with
 from prefect import task
+import prefect
 
 
 @task
 def get_mentor_excel_filepaths(dirpath: Path) -> List[Path]:
 
+    logger = prefect.context.get("logger")
+
     # get all excel spreadsheets in mentor dir but the test example ...
     # return [file for file in dirpath.rglob("*.xlsx") if file.stem not in ["test", "$~"]]
 
-    return [
-        file
-        for file in dirpath.rglob("*.xlsx")
-        if ("SEC" in file.stem)
-        and ("~$" not in file.stem)
-        and ("(1)" not in file.stem)
-        and ("(2)" not in file.stem)
-        and ("(3)" not in file.stem)
-        and ("html" not in file.stem)
+    import ipdb
+
+    ipdb.set_trace()
+
+    filepaths = [
+        filepath
+        for filepath in dirpath.rglob("*.xlsx")
+        if ("SEC" in filepath.stem)
+        and ("~$" not in filepath.stem)
+        and not bool(
+            re.search(r"\(\d\)", filepath.stem)
+        )  # ignore all SEC(1).xlsx etc. as duplicates...
+        and ("html" not in filepath.stem)
+        and ("invalid" not in filepath.stem)
     ]
+
+    if logger:
+        mentor_excels = [filepath.stem for filepath in filepaths]
+        logger.info(f"\n\nMentor Excels: {mentor_excels}\n\n")
+
+    return filepaths
 
 
 @task
