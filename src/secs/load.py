9567e9ba61fad9c0b3e3ccd134934c7b47b6f704
Rowan Molony <rowan.molony@codema.ie>
2020-07-30 13:07:57 +0100

    Rewrite load to excel as a Task class
    This enables more customization such as
    setting task dependencies which isn't easily
    achieved with FunctionTask

--- src/secs/load.py
+++ src/secs/load.py
@@ -6,24 +6,34 @@ import numpy as np
 from openpyxl import load_workbook, Workbook
 import pandas as pd
 import prefect
-from prefect import task
+from prefect import Task
 
 
-@task
-def save_to_master_excel_sheet(
-    excel_sheet: pd.DataFrame, filepath: Path, sheet_name: str, startrow: int,
-) -> pd.DataFrame:
+class SaveDataFrameToExcelSheet(prefect.Task):
+    """Saves a pandas DataFrame to a specific Excel spreadsheet
 
-    # Subtract 1 as Pandas is indexed at zero & Excel is indexed at one
-    startrow = startrow - 1
+    Parameters
+    ----------
+    Task : prefect.Task
+    """
 
-    # https://stackoverflow.com/questions/20219254/how-to-write-to-an-existing-excel-file-without-overwriting-data-using-pandas
-    book = load_workbook(filepath)
-    writer = pd.ExcelWriter(filepath, engine="openpyxl")
-    writer.book = book
-    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+    def run(
+        self,
+        excel_sheet: pd.DataFrame,
+        filepath: Path,
+        sheet_name: str,
+        startrow: int = 1,
+    ) -> pd.DataFrame:
+        # Subtract 1 as Pandas is indexed at zero & Excel is indexed at one
+        startrow = startrow - 1
 
-    excel_sheet.to_excel(
-        writer, sheet_name=sheet_name, startrow=startrow, index=False,
-    )
-    writer.save()
+        # https://stackoverflow.com/questions/20219254/how-to-write-to-an-existing-excel-file-without-overwriting-data-using-pandas
+        book = load_workbook(filepath)
+        writer = pd.ExcelWriter(filepath, engine="openpyxl")
+        writer.book = book
+        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+        excel_sheet.to_excel(
+            writer, sheet_name=sheet_name, startrow=startrow, index=False,
+        )
+        writer.save()
