9a8771440f92862d15f384a3c2d8304e5a750bfa
Rowan Molony <rowan.molony@codema.ie>
2020-07-30 10:45:16 +0100

    Raise invalid cells in Mentor Excels on read

--- src/secs/utilities.py
+++ src/secs/utilities.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 from shutil import copyfile
-from typing import Dict
+from time import sleep
+from typing import Dict, List
 
+import pandas as pd
 import prefect
 from prefect import task
+
+from secs.extract import read_excel_to_dict
 
 
 @task
@@ -14,4 +18,26 @@ def create_master_excel(template_filepath: Path, master_filepath: Path) -> None:
         logger.info(f"Skip copying as {master_filepath} already exists ...")
     else:
         logger.info(f"Copying {template_filepath} to {master_filepath} ...")
-        copyfile(template_master_excel, master_excel)
+        copyfile(template_filepath, master_filepath)
+
+
+def dataframe_contains_invalid_references(df: pd.DataFrame) -> bool:
+
+    df = df.copy()
+
+    return any(bug in df.values for bug in ["#REF!", "#VALUE!"])
+
+
+@task
+def raise_excels_with_invalid_references_in_sheets(
+    excel_filepath: Path, sheet_names: List[str],
+) -> None:
+
+    logger = prefect.context.get("logger")
+    sheets = read_excel_to_dict.run(excel_filepath)
+
+    for sheet_name in sheet_names:
+        if dataframe_contains_invalid_references(sheets[sheet_name]):
+            logger.error(
+                f"\n\n{sheet_name} in {excel_filepath} contains invalid references!\n\n"
+            )
---
+++ tests/test_utilities.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import pytest
+
+from secs.utilities import raise_excels_with_invalid_references_in_sheets
+
+INPUT_DIR = Path(__file__).parent / "input_data"
+REFERENCE_DIR = Path(__file__).parent / "reference_data"
+MENTOR_DIR = INPUT_DIR / "mentors"
+
+
+def test_raise_excels_with_invalid_references_in_sheets() -> None:
+
+    filepath_excel_with_invalid_cells = (
+        MENTOR_DIR / "DCC" / "SEC - CM - DCC invalid references.xlsx"
+    )
+
+    with pytest.raises(ValueError):
+        raise_excels_with_invalid_references_in_sheets(
+            filepath_excel_with_invalid_cells, ["Summary", "SEC contacts"]
+        )
